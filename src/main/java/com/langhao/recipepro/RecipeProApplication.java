fbba1ea33a4bf786d1a1b806f20e3faa12b5361e
Langhao Zhong <zhong74.langhao@hotmail.com>
2020-05-18 01:22:57 -0700

    Add Initial Skeleton Code

---
+++ src/main/java/com/langhao/recipepro/RecipeProApplication.java
@@ -0,0 +1,13 @@
+package com.langhao.recipepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/langhao/recipepro/RecipeProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langhao.recipepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
