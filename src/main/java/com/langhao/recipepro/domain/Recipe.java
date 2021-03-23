04f313739d172810bb02c47460ae5eac7401c6a1
Langhao Zhong <zhong74.langhao@hotmail.com>
2021-03-22 18:47:25 -0700

    Fixed Update Then Delete Failure Bug
    ABOUT: This change fixed the recipe update-delete bug. The bug can be
    replicated by first updating a recipe then attemp to delete it. Reason
    of the bug was that recipe update would create a new notes entity since
    the form would not keep the state of notes id. When deleting the recipe,
    only one note entity would be deleted because of the @OneToOne mapping,
    leaving orphan notes entities. The solution is to remove orphans in the
    entity relationship mapping and to add a hiden field in the recipeForm
    view layer to keep the state of the notes id.

--- src/main/java/com/langhao/recipepro/domain/Recipe.java
+++ src/main/java/com/langhao/recipepro/domain/Recipe.java
@@ -27,7 +27,7 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
-    @OneToOne(cascade=CascadeType.ALL)
+    @OneToOne(cascade=CascadeType.ALL, orphanRemoval=true)
     private Notes notes;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
